a8dcb68fe46612a456341e8a44bd4fa49af951fb
alex <mdafrozalam109@gmail.com>
2025-01-25 14:32:37 +0530

    variable in javascript

---
+++ lecture4/script.js
@@ -0,0 +1,34 @@
+// console.log("afroz");
+// VARIABLE IN JAVASCRIPTS///
+// there are 3type of variable in javascript
+// var,let,const,
+// var i can re-decleard,and re-define,update ,var is aglobal scope
+// scope it means u written a curly{} inside called scope//
+//let i can not re-decleard,but update easily
+//const,dont re-declear,dont update,fixed value
+//let a="10" string type value
+//let a =10 number type value
+//let a=1.234 integer type value
+//let a= true or false boolean type value
+//let a; undefine value because variable are decleared but not assign value//
+//let a=null this is not undefine because null means not null not //
+// let a = null;
+// console.log(a);
+// let b;
+// console.log(b);
+//block scope {}
+// {
+//   let a=100;
+
+// }
+// let a=100;
+// {
+//   let a=100
+// }
+//DATA Type of JS//
+// Number,String,Boolean,Integer,Char,ARRAY,Big INT
+// primative data type -> Number,null,undefine,string//
+//Non-primative data type->Array,Object//
+let a = 100;
+let b = "200";
+console.log(a + b);
