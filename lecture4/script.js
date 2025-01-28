c41023b4e42845108d1be1f5b78d6658fd46605d
alex <mdafrozalam109@gmail.com>
2025-01-28 23:49:44 +0530

    portfolio

--- lecture4/script.js
+++ lecture4/script.js
@@ -29,6 +29,104 @@
 // Number,String,Boolean,Integer,Char,ARRAY,Big INT
 // primative data type -> Number,null,undefine,string//
 //Non-primative data type->Array,Object//
-let a = 100;
-let b = "200";
-console.log(a + b);
+// let a = 100;
+// let b = "200";
+// console.log(a + b);
+// OPERATORS IN JAVASCRIPT//
+//Assign operators//
+// =,+=,-+,*=,%=, **=
+// let a =10->value of a assign 10 it means value right to left
+//comparison operators//
+//==,===,!=,!==//
+// let a = 100;
+// let b = "100";
+// if (a == b) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let A = 5;
+// let B = "5";
+// console.log(A == B);
+//true because with the help of implicit convert karta number then match A==B iske baad true result milta hai//
+// let a = 100;
+// let b = 110;
+// if (a === b) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+// let a = 100;
+// let b = "100";
+// if (a !== b) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+//Logical Operators//
+//&&,
+// let a = 200;
+// let b = 200;
+
+// if (((a == b) != b) == a) {
+//   console.log("Yes ");
+// } else {
+//   console.log("false");
+// }
+// conditional statements//
+//odd and even//
+// let num = 21;
+// //2,4,6,8,10,
+// if (num % 2 === 0) {
+//   console.log("this is odd number");
+// } else {
+//   console.log("This is a odd number");
+// }
+
+// let mode = "purple";
+// let color;
+// if (mode === "dark") {
+//   color = "green";
+// } else if (mode === "purple") {
+//   color = "pink";
+// } else if (mode === "yellow") {
+//   color = "blue";
+// } else {
+//   console.log("hii! Happy");
+// }
+// // console.log(color);
+// // loops and string//
+// let sum = 0;
+
+// for (let a = 1; a <= 100; a++) {
+//   sum += a;
+//   console.log("the counting number 1 to 10 :", a);
+// }
+// console.log(`the toata number 1 to 100: ${sum}`);
+// let a = 3;
+// while (a <= 10) {
+//   a++;
+//   console.log(a);
+// }
+// special loops //
+// for in loop
+// let fullName = "afroz";
+
+// console.log(lgt);
+// for (let name of fullName) {
+//   // console.log(name);
+//   let a = name.toUpperCase();
+//   console.log(a);
+// }
+// let fruits = ["apple", "orange"];
+// for (let fruit of fruits) {
+//   console.log(fruit[0]);
+// }
+
+let str = "abc";
+console.log(str);
+let strNew = str.toUpperCase();
+console.log(strNew);
+// console.log(str);
+// let newstr = str.toUpperCase();
+// console.log(newstr);
